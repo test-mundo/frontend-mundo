22d57adb6fbf39844d1108b71fba57ff04e736fd
luisitoinc <luis7benitez@gmail.com>
2022-06-14 14:00:07 -0400

    Primera versión prueba de frontend

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Dispositivo from './components/Dispositivos';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,8 +18,12 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+
+      <Dispositivo/>
+      
     </div>
+   
   );
 }
 
---
+++ src/components/Dispositivos.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from 'react'
+
+const Dispositivo = () => {
+    return ( <h2>"Hola Mundo"</h2> );
+}
+ 
+export default Dispositivo ;
